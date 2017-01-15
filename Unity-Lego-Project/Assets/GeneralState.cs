cb46748e8c656cd769dfc712e39e3e34f219dd9b
brochington <brochington@gmail.com>
2017-01-14 20:14:57 -0800

    Getting basic state handling.

---
+++ Unity-Lego-Project/Assets/GeneralState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralState : MonoBehaviour {
+    private static GeneralState _instance;
+
+    public static GeneralState Instance {
+        get {
+            if (_instance == null) {
+                GameObject go = new GameObject("GeneralState");
+                go.AddComponent<GeneralState>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public int myNum = 1234;
+
+    private void Awake() {
+        _instance = this;
+    }
+
+    public string legoColor = "blue";
+    public string legoShape = "1x1";
+
+    public void setLegoColor(int newNum) {
+        myNum = newNum;
+    }
+
+    public void setLegoShape(string newLegoShape) {
+        legoShape = newLegoShape;
+    }
+}
--- Unity-Lego-Project/Assets/LeftControllerInputs.cs
+++ Unity-Lego-Project/Assets/LeftControllerInputs.cs
@@ -2,16 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SteamVR_TrackedObject))]
 public class LeftControllerInputs : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("YOYOYOYOYOYOYOY");
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject generalState;
+
+    private void OnEnable() {
+       // _controller = GetComponent<SteamVR_TrackedController>();
+
+        // _controller.OnTriggerClicked = this.handleTriggerClicked;
+    }
+
+    private void OnDisable() {
+
+    }
+
+    // Use this for initialization
+    void Awake () {
+        Debug.Log("YOYOYOYOYOYOYOY");
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            // Debug.Log(GeneralState.Instance.myNum);
+            int oldNum = GeneralState.Instance.myNum;
+            GeneralState.Instance.setMyNum(++oldNum);
+            Debug.Log(GeneralState.Instance.myNum);
+            this.fartSomething();
+        }
+
+        //if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+        //    Debug.Log("Pressed up!");
+        //}
+
+        //if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+        //    Debug.Log("touch down son.");
+        //}
+
+        //if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            
+        //    if (device.GetHairTrigger()) {
+        //        Debug.Log("touch man!");
+        //    }
+        //}
+    }
+
+    private void fartSomething() {
+        GameObject thing = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        thing.AddComponent<Rigidbody>();
+        thing.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
 }
